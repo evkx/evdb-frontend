3c2cbe8f18851ac4c62698b0d26d041bd9f345d9
Rune T. Larsen <rune@hjemmekino.no>
2022-12-22 22:34:37 +0100

    update

--- src/components/EvSearchPage/EvSearchPage.tsx
+++ src/components/EvSearchPage/EvSearchPage.tsx
@@ -1,7 +1,15 @@
-import { Page, PageHeader, PageContent } from '@altinn/altinn-design-system';
+import {
+  Page,
+  PageHeader,
+  PageContent,
+  Button,
+  ButtonVariant,
+  ButtonColor,
+} from '@altinn/altinn-design-system';
 import { useTranslation } from 'react-i18next';
 
 import { ReactComponent as ApiIcon } from '@/assets/Settings.svg';
+import { ReactComponent as Cancel } from '@/assets/Cancel.svg';
 
 import classes from './EvSearchPage.module.css';
 
@@ -13,7 +21,15 @@ export const EvSearchPage = () => {
       <Page>
         <PageHeader icon={<ApiIcon />}>{t('api_delegation.api_delegations')}</PageHeader>
         <PageContent>
-          <div className={classes.pageContent}></div>
+          <div className={classes.pageContent}>
+            <Button
+              variant={ButtonVariant.Quiet}
+              color={ButtonColor.Secondary}
+              icon={<Cancel />}
+            >
+              {t('api_delegation.undo')}
+            </Button>
+          </div>
         </PageContent>
       </Page>
     </div>
