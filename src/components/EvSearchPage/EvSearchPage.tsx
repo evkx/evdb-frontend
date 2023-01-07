8e2e479c72087696d6c974ee7256aaeda2d7199f
Rune T. Larsen <rune@hjemmekino.no>
2023-01-07 21:39:51 +0100

    ev update

--- src/components/EvSearchPage/EvSearchPage.tsx
+++ src/components/EvSearchPage/EvSearchPage.tsx
@@ -5,6 +5,8 @@ import {
   Button,
   ButtonVariant,
   ButtonColor,
+  CheckboxGroup,
+  CheckboxGroupVariant,
 } from '@altinn/altinn-design-system';
 import { useTranslation } from 'react-i18next';
 
@@ -22,8 +24,22 @@ export const EvSearchPage = () => {
         <PageHeader icon={<ApiIcon />}>{t('api_delegation.api_delegations')}</PageHeader>
         <PageContent>
           <div className={classes.pageContent}>
+            <CheckboxGroup
+              data-testid='evsearch-evtype'
+              variant={CheckboxGroupVariant.Vertical}
+              items={[
+                { checked: false, label: 'SUV', name: 'suv' },
+                { checked: false, label: 'Sedan', name: 'sedan' },
+                { checked: false, label: 'CrossOver', name: 'crossover' },
+                { checked: false, label: 'MPV', name: 'mpw' },
+                { checked: false, label: 'Wagon', name: 'wagon' },
+                { checked: false, label: 'Hatchback', name: 'hatchback' },
+              ]}
+            ></CheckboxGroup>
+
+            <br></br>
             <Button
-              variant={ButtonVariant.Quiet}
+              variant={ButtonVariant.Filled}
               color={ButtonColor.Secondary}
               icon={<Cancel />}
             >
